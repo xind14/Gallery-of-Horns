1d350a63d63e66484865ab4d422f1303f75ca9f6
Xin Deng <xind1470@gmail.com>
2023-11-27 19:46:15 -0800

    Finished footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import styles from './footer.module.css'
+
+function Footer(){
+    return (
+        <footer className= {styles.foot}> &copy; 2023 Xin Deng</footer>
+    )
+}
+
+export default Footer
