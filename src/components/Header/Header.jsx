3490e2d9e66b709ef0e9527d5336577d4a55fda5
Xin Deng <xind1470@gmail.com>
2023-11-27 19:48:28 -0800

    finished header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './header.module.css' 
+
+
+
+function Header(){
+    return (
+        <header className={styles.hdr}>
+            <h1>
+                Gallery of Horns
+            </h1>     
+        </header>
+    )
+}
+
+
+export default Header;
