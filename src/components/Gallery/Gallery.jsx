94cf816c5abeec25a1cdf939ae4d9831382577f0
Xin Deng <xind1470@gmail.com>
2023-11-29 16:03:46 -0800

    Added function that changes state in App

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,29 +1,21 @@
-
 import React from "react";
 import HornedBeast from "../HornedBeasts/HornedBeasts.jsx";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 // import styles from './gallery.module.css'
 
-function Gallery(props){
-    return (
 
-// people = gallery = JSON
-// title=header 
-// members=beastGallery
-// list = gallery JSON
-// content =footer
-// map(person, index = current value, index)
-<Container>
-<Row md={2}>
+// Send a function into your Gallery component that allows the user to update state in the App
 
-        
-        {
-            props.list.map((hornedBeast, index)=>
-            <HornedBeast 
+
+function Gallery(props) {
+  return (
+    <Container>
+      <Row xs={1} sm={2} md={3} lg={4}>
+        {props.list.map((hornedBeast, index) => (
+          <HornedBeast
             key={index}
             title={hornedBeast.title}
             _id={hornedBeast._id}
@@ -31,15 +23,41 @@ function Gallery(props){
             description={hornedBeast.description}
             keyword={hornedBeast.keyword}
             horns={hornedBeast.horns}
-
-            />
-            )
-        }
-           </Row>
+            onClick={() => props.onImageClick(hornedBeast)}
+          />
+        ))}
+      </Row>
     </Container>
-
-
-    );
+  );
 }
 
 export default Gallery;
+
+
+//class demo
+// import React from 'react';
+
+// import Person from './Person.jsx';
+
+// function People(props) {
+
+// return (
+//     <>
+//     {
+//         props.list.map( (person,index) =>
+//         <Person
+//             key={index}
+//             name={person.name}
+//             hair={person.hair}
+//             votes={person.votes}
+//             handleVote={props.handleVote}
+//         />
+//         )
+//     }
+
+//     </>
+// )
+
+// }
+
+// export default People;
