0c004a7b85f0b7ebd9894d299460a1f75d932174
Xin Deng <xind1470@gmail.com>
2023-11-30 18:32:00 -0800

    task form and filter is finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import Footer from './components/Footer/Footer.jsx';
 import SelectedBeast from './components/SelectedBeast/SelectedBeast.jsx';
 import FilterBeast from './components/FilterBeast/FilterBeast.jsx';
 
+
 import gallery from './assets/data.json';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
-
 const [clickBeast, setClickBeast]=useState({});
 
 const [showModal, setShowModal]=useState(false);
 const [filterHorns, setFilterHorns] = useState('All');
+  const [galleryData, setGalleryData] = useState(gallery);
 
 const handleShowModal=(hornedBeast)=>{
   setClickBeast (hornedBeast);
@@ -25,19 +26,25 @@ const handleCloseModal=()=>{
   setShowModal(false);
 };
 
-
-const handleFilter = (value) => {
-  setFilterValue(value);
-
+// Used chatGPT for this, could not figure out how to use ? and : shown in class today
+const handleFilterHorns = (value) => {
+  // console.log('Filtering by Horns:', value);
+  setFilterHorns(value);
+  const filteredData = value === 'All'
+  ? gallery
+  : gallery.filter(hornedBeast => hornedBeast.horns ===parseInt(value) );
+  console.log('Filtered Data:', filteredData);
+  // console.log('Updated Gallery Data:', filteredData);
+setGalleryData([...filteredData]);
 };
 
 
   return(
     <main>
       <Header title="Gallery of Horns" beastGallery={gallery.length} />
-      <FilterBeast onFilterChange={handleFilterChange} />
+      <FilterBeast onFilterHorns={handleFilterHorns} />
 
-      <Gallery list ={gallery} onImageClick={handleShowModal}/>
+      <Gallery list ={galleryData} onImageClick={handleShowModal}/>
       <Footer content="&copy; 2023 Xin Deng"/>
       <SelectedBeast
       show={showModal}
@@ -53,58 +60,3 @@ export default App
 
 
 
-// Map over the JSON data in your Gallery component to render each beast
-// Send a function into your Gallery component that allows the user to update state in the App
-// Create a SelectedBeast component and include it in your App
-// Use the state in the App to render an individual beast in a Modal in the SelectedBeast component using React Bootstrap
-// When the user clicks on an individual image
-// Then the image should render larger on the screen with the description displayed
-
-//in class demo
-// import { useState } from 'react'
-
-// import Header from './components/Header.jsx';
-// import Footer from './components/Footer.jsx';
-// import People from './components/People.jsx';
-
-// import database from './assets/family.json';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// function App() {
-
-// const [people, setPeople] = useState(database);
-
-// function voteFor(person) {
-
-//     console.log('People', people);
-
-//     // Iterate the people array and find "person"
-//     let newPeople = people.map( (obj, idx) => {
-//     // Increase their vote count
-//     if( obj.name === person ) {
-//         obj.votes++;
-//         return obj;
-//     }
-//     return obj;
-//     });
-
-//     console.log("newPeople", newPeople);
-
-//     // Update State
-//     setPeople(newPeople);
-
-//     console.log(people);
-// }
-
-// return (
-//     <main>
-//     <Header title="Our Family!" members={people.length} />
-//     <People list={people} handleVote={voteFor} />
-//     <Footer content="Copyright 2023: John and the 301's" />
-//     </main>
-// )
-
-// }
-
-// export default App;
--- src/components/FilterBeast/FilterBeast.jsx
+++ src/components/FilterBeast/FilterBeast.jsx
@@ -5,17 +5,19 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 function FilterBeast(props) {
-  const handleFilterChange = (event) => {
-    // Call a function in the parent component (e.g., App) to handle the filter change
-    props.onFilterChange(event.target.value);
+  const handleFilterHorns = (event) => {
+    event.preventDefault();
+    props.onFilterHorns(event.target.value);
   };
 
+  
+
   return (
-    <Form>
+    <Form >
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridHorns">
           <Form.Label>Filter by Horns:</Form.Label>
-          <Form.Select defaultValue="All" onChange={handleFilterChange}>
+          <Form.Select defaultValue="All" onChange={handleFilterHorns}>
             <option value="All">All</option>
             <option value="1">One Horn</option>
             <option value="2">Two Horns</option>
@@ -24,10 +26,6 @@ function FilterBeast(props) {
           </Form.Select>
         </Form.Group>
       </Row>
-
-      <Button variant="primary" type="submit">
-        Apply Filter
-      </Button>
     </Form>
   );
 }
